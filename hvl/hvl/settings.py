bbdc7afa7b9f58d9153fdadd5579cb2fd34b47b0
Pawel Mazurek <pawel_mazurek1@hotmail.com>
2021-08-22 11:26:34 +0200

    Add Django project boilerplate

--- hvl/hvl/settings.py
+++ hvl/hvl/settings.py
@@ -9,21 +9,24 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
-
+import os
 from pathlib import Path
+
+import dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+dotenv.load_dotenv()
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-+=w5$-p=83v4-w%jgqke)=ak^ctgq1-#($zz1ll_f+k%w43k*t'
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv("DEBUG") == "True"
 
 ALLOWED_HOSTS = []
 
