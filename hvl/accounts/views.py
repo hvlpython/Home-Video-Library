f6e8ed64ac54df275df333b1a092170976aed6ea
Pawel Mazurek <pawel_mazurek1@hotmail.com>
2021-08-24 13:03:18 +0200

    Add user login.html, user logout to base.html and superuser auth for registration

--- hvl/accounts/views.py
+++ hvl/accounts/views.py
@@ -1,10 +1,13 @@
 from django.shortcuts import render
 from django.views.generic.edit import CreateView
 from django.contrib.auth.forms import UserCreationForm 
+from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin, UserPassesTestMixin
 
 
-# Create your views here.
+class IsSuperuserMixin(UserPassesTestMixin):
+    def test_func(self):
+        return self.request.user.is_superuser
 
-class UserRegisterView(CreateView):
+class UserRegisterView(IsSuperuserMixin, CreateView):
     form_class = UserCreationForm
     template_name= "registration/register.html"
