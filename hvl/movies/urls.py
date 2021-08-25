97a25dad0cc1645d44f6f48cd49b49c46a79b8c9
marsta666 <marcel.stala@gmail.con>
2021-08-25 21:29:32 +0200

    missing files

---
+++ hvl/movies/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from movies.views import MovieCreateView, MovieListView, MovieUpdateView, MovieDeleteView
+
+urlpatterns = [
+    path('movie-create/', MovieCreateView.as_view(), name='movie-create'),
+    path('movie-list/', MovieListView.as_view(), name='movie-list'),
+    path('movie-update/<pk>/', MovieUpdateView.as_view(), name='movie-update'),
+    path('movie-delete/<pk>/', MovieDeleteView.as_view(), name='movie-delete'),
+    ]
