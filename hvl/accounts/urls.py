59afd480d4d9f8e75e1f49619f3481e4c8358800
Pawel Mazurek <pawel_mazurek1@hotmail.com>
2021-08-25 14:43:23 +0200

    Add edit profile and password change

--- hvl/accounts/urls.py
+++ hvl/accounts/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
-from accounts.views import UserRegisterView
+from accounts.views import UserRegisterView, UserEditView, PasswordsChangeView, password_success
+
 
 urlpatterns = [
     path('register/', UserRegisterView.as_view(), name='register'),
-    
+    path('edite-profile/', UserEditView.as_view(), name='edit-profile'),
+    path('password/', PasswordsChangeView.as_view(), name='change-password'),
+    path('password-success/', password_success, name='password-success')
 ]
--- hvl/accounts/views.py
+++ hvl/accounts/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
-from django.views.generic.edit import CreateView
+from django.views.generic.edit import CreateView, UpdateView
 from django.contrib.auth.mixins import UserPassesTestMixin
-from accounts.forms import UserRegisterForm
+from django.contrib.auth.views import PasswordChangeView
+from accounts.forms import UserRegisterForm, UserEditProfileForm, UserEditPasswordForm
+from django.urls import reverse_lazy
 
 class IsSuperuserMixin(UserPassesTestMixin):
     def test_func(self):
@@ -10,3 +12,18 @@ class IsSuperuserMixin(UserPassesTestMixin):
 class UserRegisterView(IsSuperuserMixin, CreateView):
     form_class = UserRegisterForm
     template_name= "registration/register.html"
+
+class UserEditView(UpdateView):
+    form_class = UserEditProfileForm
+    template_name= "registration/edit-profile.html"
+
+    def get_object(self):
+        return self.request.user
+
+class PasswordsChangeView(PasswordChangeView):
+    form_class = UserEditPasswordForm
+    template_name = "registration/change-password.html"
+    success_url = reverse_lazy('password-success')
+
+def password_success(request):
+    return render(request, 'registration/password-success.html')
