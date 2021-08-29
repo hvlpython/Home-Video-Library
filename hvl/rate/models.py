3a7022dc0e0a18f92448ba973c6d2b658efb5f99
tomasek <tomasekpwr@gmail.com>
2021-08-29 14:04:05 +0200

    create model rating

--- hvl/rate/models.py
+++ hvl/rate/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.core.validators import  MaxValueValidator, MinValueValidator
 
-# Create your models here.
+from movies.models import Movie
+
+
+class Rating(models.Model):
+    movie = models.ForeignKey(Movie , on_delete=models.CASCADE)
+    rating = models.IntegerField(default=0, validators=[MaxValueValidator(5), MinValueValidator(0)])
+
+    def __str__(self):
+        return f"{self.rating}"
