bea12eb7f8c6c7f270521cb5d1d13a7033550fd1
tomasek <tomasekpwr@gmail.com>
2021-08-29 14:33:30 +0200

    create RatingView

--- hvl/rate/urls.py
+++ hvl/rate/urls.py
@@ -1,1 +1,7 @@
 from django.urls import path
+from rate.views import RatingCreateView
+
+
+urlpatterns = [
+    path('rate/', RatingCreateView.as_view(), name='rate'),
+    ]
--- hvl/rate/views.py
+++ hvl/rate/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from django.views.generic import CreateView
+from rate.forms import RatingForm
 
 class RatingCreateView(CreateView):
+    form_class = RatingForm
     template_name = "create.html"
