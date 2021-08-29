f6c260253740bf5a23b27f41a5693dca436bebc4
tomasek <tomasekpwr@gmail.com>
2021-08-29 14:49:02 +0200

    register RAting in admin

--- hvl/rate/views.py
+++ hvl/rate/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.views.generic import CreateView
 from rate.forms import RatingForm
 
+
 class RatingCreateView(CreateView):
     form_class = RatingForm
     template_name = "create.html"
