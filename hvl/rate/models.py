0d90ee7b78fb1d361dcde4be0f8df53f4d63b723
tomasek <tomasekpwr@gmail.com>
2021-08-30 14:19:56 +0200

    add coments

--- hvl/rate/models.py
+++ hvl/rate/models.py
@@ -8,5 +8,9 @@ class Rating(models.Model):
     movie = models.ForeignKey(Movie, on_delete=models.CASCADE)
     rating = models.IntegerField(default=0, validators=[MaxValueValidator(5), MinValueValidator(0)])
 
+    # # unique, it's probably bad
+    # class Meta:
+    #     unique_together = ('rating', 'movie')
+
     def __str__(self):
         return f"{self.rating}"
