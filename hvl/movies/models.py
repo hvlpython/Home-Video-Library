c7904d6efdff735cbce4cb62ab9e453a7e24b1e2
tomasek <tomasekpwr@gmail.com>
2021-09-03 18:54:49 +0200

    add  average value

--- hvl/movies/models.py
+++ hvl/movies/models.py
@@ -2,6 +2,8 @@ from django.db import models
 from django.db.models import Avg
 
 
+
+
 class Genre(models.Model):
     name = models.CharField(max_length=128)
 
@@ -18,21 +20,22 @@ class Movie(models.Model):
     movie_image = models.ImageField(blank=True, null=True, upload_to="images/mowies-image")
 
     #zmiany
-    @property
-    def average_rating(self):
-        data = self.movie.all().aggregate(Avg('rating')).get('rating__avg', 0.00)
-        print(data)
-        return data
+    # @property
+    # def average_rating(self):
+    #     data = self.movie.all().aggregate(Avg('rating')).get('rating__avg', 0.00)
+    #     print(data)
+    #     return data
         # Change 0.00 to whatever default value you want when there
         # are no reviews.
-
-    # def get_avg_rating(self):
-    #     reviews = Rating.objects.filter(product=self)
-    #     count = len(reviews)
-    #     sum = 0
-    #     for rvw in reviews:
-    #         sum += rvw.rating
-    #     return (sum / count)
+    @property
+    def get_avg_rating(self):
+        from rate.models import Rating
+        reviews = Rating.objects.filter(movie=self)
+        count = len(reviews)
+        sum = 0
+        for rvw in reviews:
+            sum += rvw.rating
+        return (sum / count)
     #///
     def __str__(self):
         return f"{self.title}"
