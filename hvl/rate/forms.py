f74879dbac1df8bb320cd67b141772be340d593d
Pawel Mazurek <pawel_mazurek1@hotmail.com>
2021-09-05 11:22:06 +0200

    Add clean code

--- hvl/rate/forms.py
+++ hvl/rate/forms.py
@@ -8,7 +8,6 @@ class RatingForm(forms.Form):
     rating = forms.IntegerField()
     movie = forms.IntegerField(widget=HiddenInput())
 
-
     def __init__(self, *args, instance=None, **kwargs):
         super().__init__(*args, **kwargs)
         print(instance)
